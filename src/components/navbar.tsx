4c9e04234bcf96a610fcc5b91a8542c90819a5f4
Allen E <evangelista.davidallen2003@gmail.com>
2025-12-31 17:28:58 +0800

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,17 @@
 // src/components/Navbar.tsx
+
+import { Link } from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between bg-white shadow px-6 py-4 sticky top-0 z-50">
       <h1 className="text-2xl font-bold text-blue-600">TechnoBitz</h1>
       <ul className="flex items-center space-x-6">
-        <li><a href="/" className="hover:text-blue-600">Home</a></li>
-        <li><a href="/cart" className="hover:text-blue-600">Cart</a></li>
-        <li><a href="/products" className="hover:text-blue-600">Products</a></li>
-         <li><a href="/login" className="hover:text-blue-600">Log In</a></li>
-          <li><a href="/signup" className="hover:text-blue-600">Sign Up</a></li>
+        <li><Link href="/" className="hover:text-blue-600">Home</Link></li>
+        <li><Link href="/cart" className="hover:text-blue-600">Cart</Link></li>
+        <li><Link href="/products" className="hover:text-blue-600">Products</Link></li>
+         <li><Link href="/login" className="hover:text-blue-600">Log In</Link></li>
+          <li><Link href="/signup" className="hover:text-blue-600">Sign Up</Link></li>
       </ul>
     </nav>
   );
